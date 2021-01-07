31dfa2d26f19c1f9785d34d4063db7ffc8e7a2c1
aldiariq <aldiariq@github.com>
2021-01-07 11:05:29 +0700

    selesai fitur hapus materi

--- router/routerKelolamateri.js
+++ router/routerKelolamateri.js
@@ -17,6 +17,8 @@ const uploadfile = multer({ storage: konfigurasipenyimpanan })
 const modelMateri = require('../model/modelMateri');
 const modelPengguna = require('../model/modelPengguna');
 
+const filesystem = require('fs');
+
 //implementasi router
 router.post('/materi', validasiToken, uploadfile.any(), async (req, res) => {
     //tampung error inputan
@@ -123,6 +125,64 @@ router.get('/materi/:_id', validasiToken, async (req, res) => {
         });
     }
 
+});
+
+router.delete('/materi/:_id', validasiToken, async (req, res) => {
+    //tampung data pengguna
+    const datapengguna = await modelPengguna.find({
+        _id: req.pengguna._id,
+        email: req.pengguna.email
+    });
+
+    //pengecekan data pengguna
+    if (datapengguna) {
+        //tampung data materi
+        const datamateri = await modelMateri.findOne({
+            _id: req.params._id,
+            idpengguna: req.pengguna._id
+        })
+
+        //pengecekan data materi
+        if (datamateri) {
+            try {
+                //proses penghapusan data materi
+                const hapusmateri = await modelMateri.deleteOne({
+                    _id: req.params._id,
+                    idpengguna: req.pengguna._id
+                });
+
+                //pengecekan hapus data materi
+                if (hapusmateri) {
+                    filesystem.unlinkSync(process.env.LOKASIFILE + datamateri.namafilemateri);
+
+                    return res.status(200).send({
+                        'status': true,
+                        'pesan': 'Berhasil Menghapus Data Materi'
+                    });
+                } else {
+                    return res.status(400).send({
+                        'status': false,
+                        'pesan': 'Gagal Menghapus Data Materi'
+                    });
+                }
+            } catch (error) {
+                return res.status(400).send({
+                    'status': false,
+                    'pesan': 'Gagal Menghapus File Materi'
+                });
+            }
+        } else {
+            return res.status(400).send({
+                'status': false,
+                'pesan': 'Materi Tidak Ditemukan'
+            });
+        }
+    } else {
+        return res.status(400).send({
+            'status': false,
+            'pesan': 'Pengguna Tidak Terdaftar'
+        });
+    }
 });
 
 
